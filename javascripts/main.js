a732a2450daa35f4f051a2b0186d3236b6b3bf4c
Jean-Eudes <jeudes7@gmail.com>
2013-12-04 12:56:51 +0100

    remove console

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,3 @@
-console.log('This would be the main JS file.');
-
 jQuery(document).ready(function() {
   jQuery(".solution").wrap("<div class = 'spoiler'></div>")
   jQuery(".solution").before("<a class = 'teaser'>Afficher la solution</a>")
